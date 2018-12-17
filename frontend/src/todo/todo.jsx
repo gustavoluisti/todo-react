fad1cad8a2c1fbfa1fbebd7ae77847cbd8f3f4e6
Luis Gustavo Santos <luisgustavosantos@spartas-MacBook-Pro.local>
2018-12-16 23:00:00 -0200

    React

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import PageHeader from '../template/pageHeader'
+import TodoForm from '../todo/todoForm'
+import TodoList from '../todo/todoList'
+
+export default class Todo extends Component {
+    handleAdd(){
+        console.log(this)
+    }
+    render(){
+        return(
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoList />
+            </div>
+        )
+    }
+}
